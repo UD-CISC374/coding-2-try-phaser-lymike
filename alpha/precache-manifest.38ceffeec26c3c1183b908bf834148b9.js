cd64ae14622e5a9e5fc04959ba86ef6edf372de0
Michael Ly <lymike@udel.edu>
2020-04-18 00:28:55 -0400

    Updates

---
+++ alpha/precache-manifest.38ceffeec26c3c1183b908bf834148b9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-lymike/alphaassets/citations.md"
+  },
+  {
+    "revision": "c779c056b72036843cc113678c5e2b78",
+    "url": "/coding-2-try-phaser-lymike/alphaassets/images/background.jpg"
+  },
+  {
+    "revision": "2aa3455b9c25db057cc2a9837530acae",
+    "url": "/coding-2-try-phaser-lymike/alphaassets/images/enemy1.png"
+  },
+  {
+    "revision": "71b0b9014e89e801e1b09559e5250121",
+    "url": "/coding-2-try-phaser-lymike/alphaassets/images/enemy2.png"
+  },
+  {
+    "revision": "29f73f18b12eae24d9c0434ac9305407",
+    "url": "/coding-2-try-phaser-lymike/alphaassets/images/enemy3.png"
+  },
+  {
+    "revision": "29c4451a9db5801a2c46baccf9ffa35b",
+    "url": "/coding-2-try-phaser-lymike/alphaassets/images/ground.png"
+  },
+  {
+    "revision": "c79012b06900f1596a741e7f36146078",
+    "url": "/coding-2-try-phaser-lymike/alphaassets/images/play_button.png"
+  },
+  {
+    "revision": "20d454d3d94da42b06ac3122b7926f2f",
+    "url": "/coding-2-try-phaser-lymike/alphaassets/images/player.png"
+  },
+  {
+    "revision": "07e5dbf12aab4d460c34",
+    "url": "/coding-2-try-phaser-lymike/alphagen.main.b7e09663bcf0382609f8.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-lymike/alphagen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "baf15e3b768048e23e831ef9bfa224e7",
+    "url": "/coding-2-try-phaser-lymike/alphaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-lymike/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-lymike/alphasw.js"
+  }
+]);
